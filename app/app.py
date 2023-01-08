dd368d3665afea09260d87bfa9108ca99654bcd1
Kevin Huang <33188761+KevinH48264@users.noreply.github.com>
2023-01-07 19:49:38 -0500

    readded requirements.txt

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS, cross_origin
 import requests
-import retriever
+import app.retriever as retriever
 
 app = Flask(__name__)
 cors = CORS(app)
